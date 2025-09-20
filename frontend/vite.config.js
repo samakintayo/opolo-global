15ac1b3907ea1d9e10f6e1f45311cd0bd50bc352
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-09-20 12:34:32 +0100

    j

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
   },
-  base: '/',
-  server: {
-    proxy: {
-      "/api": "http://localhost:4000"
-    }
-  }
-})
+  base: './'
+});
