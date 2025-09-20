cd25906576bf6d2338fec5fbfe02e407c8af85b7
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-09-20 14:14:45 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,17 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import PaymentStatus from "./paymentStatus";
-import "./index.css"
-import AiBootcamp from "./programme/AiBootcamp";
 import Register from "./register";
-
-
+import AiBootcamp from "./programme/AiBootcamp";
+import PaymentStatus from "./paymentStatus";
 
 function App() {
   return (
-    <>
-      <Router>
-             <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/programme/ai-bootcamp" element={<AiBootcamp />} />
-        <Route path="/payment-status" element={<PaymentStatus />} />
-      </Routes>
-    </Router>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/programme/ai-bootcamp" element={<AiBootcamp />} />
+      <Route path="/payment-status" element={<PaymentStatus />} />
+    </Routes>
   );
 }
 
