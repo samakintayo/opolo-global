f228d14e978c5b9cec63457712d4d126fdb70733
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-09-20 13:23:38 +0100

    jjjjjjj

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/dist'
+    outDir: "dist",   // relative path to frontend folder ✅
+    emptyOutDir: true // ensures old files are cleared
   },
-  base: './'
+  base: "./"
 });
