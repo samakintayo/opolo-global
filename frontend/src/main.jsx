862a7244aa3611f8ddc81326c9d52e931b74d039
Akinnyajo Akintayo <65978922+samakintayo@users.noreply.github.com>
2025-09-20 14:13:42 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // <-- add this
+//import { BrowserRouter } from "react-router-dom"; // <-- add this
 import App from "./App";
 import "./index.css"; // optional Tailwind import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+
       <App />
-    </BrowserRouter>
+
   </React.StrictMode>
 );
