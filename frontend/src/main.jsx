bdd6efceef5ee02c81b92748f7b1c4c66ee6ec55
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-09-20 12:41:35 +0100

    jjj

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // optional Tailwind import
+
+
+// filepath: c:\Users\AkintayoPC\Documents\ReactApp\Opolo.global\opologlobal\frontend\src\main.jsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
