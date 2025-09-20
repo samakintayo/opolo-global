eedddc4dbd252d2c66d7706baa854e7968388e62
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-09-20 14:34:53 +0100

    hhhh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Register from "./register";
 import AiBootcamp from "./programme/AiBootcamp";
 import PaymentStatus from "./paymentStatus";
 
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/programme/ai-bootcamp" element={<AiBootcamp />} />
-      <Route path="/payment-status" element={<PaymentStatus />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/programme/ai-bootcamp" element={<AiBootcamp />} />
+        <Route path="/payment-status" element={<PaymentStatus />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
